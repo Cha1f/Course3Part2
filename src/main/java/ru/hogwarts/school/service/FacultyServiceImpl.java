76bfb30be74c1b1ece82a16d333e0184a73a8f08
Рома <pomo4444ka@gmail.com>
2023-08-15 01:45:25 +0300

    5. Изменил также факультиСервис и добавил зависимость

--- src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
@@ -1,45 +1,43 @@
 package ru.hogwarts.school.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.repositories.FacultyRepository;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Objects;
 
 @Service
 public class FacultyServiceImpl implements FacultyService {
-    private final HashMap<Long, Faculty> faculties = new HashMap<>();
-    private long count = 0;
 
+    private final FacultyRepository facultyRepository;
+
+    public FacultyServiceImpl(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
 
     public Faculty addFaculty(Faculty faculty) {
-        faculty.setId(count++);
-        faculties.put(faculty.getId(), faculty);
-        return faculty;
+        return facultyRepository.save(faculty);
     }
 
     public Faculty findFaculty(long id) {
-        return faculties.get(id);
+        return facultyRepository.findById(id).get();
     }
 
     public Faculty editFaculty(long id, Faculty faculty) {
-        if (!faculties.containsKey(id)) {
-            return null;
-        }
-        faculties.put(id, faculty);
-        return faculty;
+        return facultyRepository.save(faculty);
     }
 
     public void deleteFaculty(long id) {
-        faculties.remove(id);
+        facultyRepository.deleteById(id);
     }
 
 
     public Collection<Faculty> findByColor(String color) {
         ArrayList<Faculty> result = new ArrayList<>();
-        for (Faculty faculty : faculties.values()) {
+        for (Faculty faculty : facultyRepository.findAll()) {
             if (Objects.equals(faculty.getColor(), color)) {
                 result.add(faculty);
             }
